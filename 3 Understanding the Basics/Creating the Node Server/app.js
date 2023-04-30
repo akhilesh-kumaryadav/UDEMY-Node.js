252d4fff6a5e500b9f0bf446021f8e6c5c5230da
akhilesh-kumar-yadav <akhileshkumaryadav2407@gmail.com>
2023-05-01 00:07:32 +0530

    3 - Understanding the Basics / Parsing Request Bodies

--- 3 Understanding the Basics/Creating the Node Server/app.js
+++ 3 Understanding the Basics/Creating the Node Server/app.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
