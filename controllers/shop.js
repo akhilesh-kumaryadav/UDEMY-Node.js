12ce329fb25107b9942559e72ef7a6b5ff0e637c
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2023-12-05 00:23:26 +0530

    Rendering the Product Detail View

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,8 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "All Products",
       path: "/products",
