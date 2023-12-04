1b5bfdcb5109eee533371289a3f2be1f2bd86dfe
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2023-12-04 16:50:06 +0530

    Start of the "Enhancing the app"

--- 8 Enhacing the app/models/product.js
+++ 8 Enhacing the app/models/product.js
@@ -1,30 +1,40 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
-  path.dirname(require.main.filename),"data","product.json"
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) products = JSON.parse(fileContent);
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) return cb([]);
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
