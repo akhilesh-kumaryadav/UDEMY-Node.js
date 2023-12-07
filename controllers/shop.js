52ac001f473c29743e63299f978f6f3823b51daa
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2023-12-07 18:59:38 +0530

    131. Deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,7 +48,7 @@ exports.getCart = (req, res, next) => {
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
-        products : cartProducts
+        products: cartProducts,
       });
     });
   });
@@ -60,6 +60,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect("/cart");
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
 };
 
 exports.getOrders = (req, res, next) => {
