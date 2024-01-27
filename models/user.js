38e33bb12bd1dbddbb4232a3e29408444b876c7c
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2024-01-27 13:14:04 +0530

    195. Storing the User in our Database

--- models/user.js
+++ models/user.js
@@ -1,23 +1,29 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email){
-    this.name = username,
-    this.email = email
+  constructor(username, email) {
+    (this.name = username), (this.email = email);
   }
 
-  save(){
+  save() {
     const db = getDb();
-    return db.collection("users").insertOne(this)
+    return db.collection("users").insertOne(this);
   }
 
-  static findById(userId){
+  static findById(userId) {
     const db = getDb();
-    return db.collection("users").findOne   ({_id : new ObjectId(userId)})
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => console.log(err));
   }
 }
 
-module.exports = User; 
+module.exports = User;
