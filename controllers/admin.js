965766c5421dac98a4684a39e41bdfe47e30c069
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2024-01-28 17:28:07 +0530

    216. Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,15 +57,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDescription,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
     .then((result) => {
       console.log("Product Updated!!!");
       res.redirect("/admin/products");
@@ -74,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
