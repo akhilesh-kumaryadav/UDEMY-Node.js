4c4eeb6b813ff149e3dacb47ad3ec8d5e7d1a71b
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2024-02-12 21:55:08 +0530

    298. Keeping User Input

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,12 +43,27 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = exValidatorResult(req);
+  if (!errors.isEmpty()) {
+    //console.log(errors.array());
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   User.findOne({ email: email })
     .then((user) => {
@@ -91,6 +106,11 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword,
+      },
     });
   }
 
